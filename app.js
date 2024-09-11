6e459b391e74ae666bcb1143948a60b41d6ea19f
fawaz <fawazogunleye@outlook.com>
2024-09-11 16:53:58 -0400

    send

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { botStart } = require('./src/controllers/bot'); 
 const mongoose = require('mongoose');
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 mongoose.connect('mongodb+srv://fawazogunleye:Aabimbola2022@cluster0.caz9xfe.mongodb.net/heirstonvolt?retryWrites=true&w=majority&appName=Cluster0');
 
-const { botStart } = require('./src/controllers/bot'); // Ensure correct path
+app.use(bodyParser.json());
 botStart();
 
 app.listen('3000', () => {
