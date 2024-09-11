59749bafca0f0fa69d661ec3be8a602621c127c5
fawaz <fawazogunleye@outlook.com>
2024-09-11 15:18:13 -0400

    send

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const app = express();
+const mongoose = require('mongoose');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+mongoose.connect('mongodb+srv://fawazogunleye:Aabimbola2022@cluster0.caz9xfe.mongodb.net/appheirstonhon?retryWrites=true&w=majority&appName=Cluster0');
 const {botStart} = require('./src/controllers/bot');
-
 botStart();
 
 
