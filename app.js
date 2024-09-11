127e27639d34e927a9bc8c59b3cdcead719779d2
fawaz <fawazogunleye@outlook.com>
2024-09-11 15:31:30 -0400

    bot integration

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-mongoose.connect('mongodb+srv://fawazogunleye:Aabimbola2022@cluster0.caz9xfe.mongodb.net/appheirstonhon?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect('mongodb+srv://fawazogunleye:Aabimbola2022@cluster0.caz9xfe.mongodb.net/heirstonvolt?retryWrites=true&w=majority&appName=Cluster0');
 const {botStart} = require('./src/controllers/bot');
 botStart();
 
