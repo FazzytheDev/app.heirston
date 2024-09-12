1229ec933000c6080991a9044af4570368f11543
fawaz <fawazogunleye@outlook.com>
2024-09-12 12:22:38 -0400

    send

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const { setupBotCommands } = require('./src/controllers/bot');
